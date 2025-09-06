493ddeb417f90d054e8fdfdb496fe305ef2c9545
JudaNanaa <moussaadamou898@gmail.com>
2025-09-06 20:32:16 +0200

    manage overflow calloc

--- srcs/calloc.c
+++ srcs/calloc.c
@@ -1,6 +1,17 @@
 #include "../includes/malloc_internal.h"
-#include <stdio.h>
-#include <unistd.h>
+#include <stdbool.h>
+
+bool is_gonna_overflow(size_t nmemb, size_t size)
+{
+	size_t check_overflow;
+
+	check_overflow = nmemb * size;
+
+	if (nmemb != 0 && check_overflow / nmemb != size) {
+		return true;
+	}
+	return false;
+}
 
 void	*calloc_internal(size_t nmemb, size_t size)
 {
@@ -14,6 +25,20 @@ void	*calloc_internal(size_t nmemb, size_t size)
 
 void *calloc_wrapper(size_t nmemb, size_t size, const char *file, int line, const char *func) {
 	void *ptr;
+
+    if (is_gonna_overflow(nmemb, size)) {
+        if (g_malloc.verbose) {
+            ft_printf_fd(STDERR_FILENO,
+        "[ERROR] calloc overflow detected at %s:%d in %s\n\t-> nmemb: %u, size: %u (multiplication too large)\n",
+                file, line, func, nmemb, size);
+        }
+        if (g_malloc.trace_file_fd != -1) {
+            ft_printf_fd(g_malloc.trace_file_fd,
+                "[ERROR] calloc overflow at %s:%d in %s (nmemb: %u, size: %u)\n",
+                file, line, func, nmemb, size);
+        }
+        return NULL;
+    }
 
 	ptr = calloc_internal(nmemb, size);
     if (g_malloc.verbose) {
